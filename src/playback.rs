dedc7cd189112333018d119c7e6e8d09e3adaaf3
Earthgames <earthgame_s@outlook.com>
2024-09-10 09:37:41 +0200

    changed things in playback - added helper stuct or PlaybackDaemon for resampling - added a new and default for PlaybackDaemon - changed the queue to a Queue

--- src/playback.rs
+++ src/playback.rs
@@ -11,6 +11,7 @@ use rubato::{FftFixedInOut, Resampler};
 
 use crate::audio_conversion::{interleaved_to_planar, planar_to_interleaved};
 use crate::decoders::{opus_decoder::OpusReader, symphonia_wrap::SymphoniaWrapper, Decoder};
+use crate::queue::Queue;
 
 #[derive(Debug)]
 pub enum PlaybackAction {
@@ -27,20 +28,46 @@ pub enum PlaybackAction {
 
 pub struct PlaybackDaemon {
     pub playing: bool,
-    _current: PathBuf,
-    _queue: Vec<PathBuf>,
+    pub queue: Queue,
     decoder: Decoder,
     left: u64,
-    resampler: FftFixedInOut<f32>,
-    buffer_input_resampler: Vec<Vec<f32>>,
-    buffer_output_resampler: Vec<Vec<f32>>,
-    buffer_decoder_output: Vec<f32>,
-    buffer_resampler_interleaved: Vec<f32>,
+    resampler: PlaybackResampler,
     buffer_output: VecDeque<f32>,
     volume_level: f32,
 }
 
+/// Helper struct for PlaybackDaemon
+/// Only contains buffers that are dependent on the decoder sample rate
+/// and the resampler itself
+struct PlaybackResampler {
+    fixed_in_out_resampler: FftFixedInOut<f32>,
+    decoder_output: Vec<f32>,
+    /// Input Resampler
+    input: Vec<Vec<f32>>,
+    /// Output Resampler
+    output: Vec<Vec<f32>>,
+    interleaved: Vec<f32>,
+}
+
 impl PlaybackDaemon {
+    pub fn new() -> PlaybackDaemon {
+        PlaybackDaemon {
+            playing: false,
+            queue: Queue::new(),
+            decoder: Decoder::None,
+            left: 0,
+            resampler: PlaybackResampler {
+                fixed_in_out_resampler: FftFixedInOut::new(1, 1, 0, 0).expect("Should be fine"),
+                decoder_output: vec![],
+                input: vec![],
+                output: vec![],
+                interleaved: vec![],
+            },
+            buffer_output: VecDeque::new(),
+            volume_level: 0.0,
+        }
+    }
+
     pub fn try_new(
         file: &str,
         sample_rate_output: usize,
@@ -58,24 +85,25 @@ impl PlaybackDaemon {
         )
         .ok()?;
         // Buffers
-        let buffer_input_resampler = resampler.input_buffer_allocate(true);
-        let buffer_output_resampler = resampler.output_buffer_allocate(true);
-        let buffer_decoder_output: Vec<f32> =
+        let input_resampler = resampler.input_buffer_allocate(true);
+        let output_resampler = resampler.output_buffer_allocate(true);
+        let decoder_output: Vec<f32> =
             vec![Sample::EQUILIBRIUM; resampler.input_frames_max() * decoder.channels()];
-        let buffer_resampler_interleaved: Vec<f32> =
+        let resampler_interleaved: Vec<f32> =
             vec![Sample::EQUILIBRIUM; resampler.output_frames_max() * decoder.channels()];
 
         Some(PlaybackDaemon {
             playing: true,
-            _current: current,
-            _queue: vec![],
+            queue: Queue::new(),
             decoder,
             left,
-            resampler,
-            buffer_input_resampler,
-            buffer_output_resampler,
-            buffer_decoder_output,
-            buffer_resampler_interleaved,
+            resampler: PlaybackResampler {
+                fixed_in_out_resampler: resampler,
+                decoder_output,
+                input: input_resampler,
+                output: output_resampler,
+                interleaved: resampler_interleaved,
+            },
             buffer_output: VecDeque::new(),
             volume_level,
         })
@@ -96,24 +124,11 @@ impl PlaybackDaemon {
     }
 
     fn add_buffer(&mut self) -> Result<()> {
-        self.left = self.decoder.fill(&mut self.buffer_decoder_output)?;
-        interleaved_to_planar(
-            &self.buffer_decoder_output,
-            &mut self.buffer_input_resampler,
-            self.decoder.channels(),
-        );
-        self.resampler.process_into_buffer(
-            &self.buffer_input_resampler,
-            &mut self.buffer_output_resampler,
-            None,
-        )?;
-        planar_to_interleaved(
-            &self.buffer_output_resampler,
-            &mut self.buffer_resampler_interleaved,
-            self.decoder.channels(),
-        );
-        self.buffer_output
-            .extend(self.buffer_resampler_interleaved.iter());
+        self.left = self.decoder.fill(&mut self.resampler.decoder_output)?;
+
+        self.resampler.resample(self.decoder.channels())?;
+
+        self.buffer_output.extend(self.resampler.interleaved.iter());
         Ok(())
     }
 
@@ -131,6 +146,25 @@ impl PlaybackDaemon {
 
     pub fn left(&self) -> u64 {
         self.left
+    }
+}
+
+impl PlaybackResampler {
+    fn resample(&mut self, channels: usize) -> Result<()> {
+        interleaved_to_planar(&self.decoder_output, &mut self.input, channels);
+
+        self.fixed_in_out_resampler
+            .process_into_buffer(&self.input, &mut self.output, None)?;
+
+        planar_to_interleaved(&self.output, &mut self.interleaved, channels);
+
+        Ok(())
+    }
+}
+
+impl Default for PlaybackDaemon {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
