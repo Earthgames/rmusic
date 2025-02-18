fc53425e627c18e3f7faa2338a83a07ec8731e32
earthgame_s@outlook.com <arend.bos@gmail.com>
2025-02-18 16:19:33 +0100

    Better context error

--- src/database/context.rs
+++ src/database/context.rs
@@ -2,7 +2,7 @@ use std::{collections::VecDeque, path::PathBuf};
 
 use entity::{artist, release, track, track_location};
 use log::{error, warn};
-use sea_orm::{prelude::*, QuerySelect, SelectColumns};
+use sea_orm::prelude::*;
 
 use crate::queue::{QueueItem, QueueOptions};
 
@@ -13,47 +13,53 @@ pub trait GetContext {
     fn get_context(
         &self,
         library: &Library,
-    ) -> impl std::future::Future<Output = Result<Option<QueueItem>>> + Send;
+    ) -> impl std::future::Future<Output = TrackResult<QueueItem>> + Send;
     // get the context when this item is played from a list of items
     fn get_context_from_list(
         self_list: Vec<&Self>,
         index: usize,
         library: &Library,
-    ) -> impl std::future::Future<Output = Result<Option<QueueItem>>> + Send;
+    ) -> impl std::future::Future<Output = TrackResult<QueueItem>> + Send;
 }
 
 impl GetContext for track::Model {
-    async fn get_context(&self, library: &Library) -> Result<Option<QueueItem>> {
-        library
+    async fn get_context(&self, library: &Library) -> TrackResult<QueueItem> {
+        match library
             .model_related::<_, track_location::Entity>(self)
-            .await
-            .map(|option| match option {
-                Some(location) => Some(QueueItem::Track(PathBuf::from(location.path))),
-                None => {
-                    warn!("Could not find path for {}", self.name);
-                    None
-                }
-            })
+            .await?
+        {
+            Some(location) => Ok(QueueItem::Track(PathBuf::from(location.path))),
+            None => {
+                warn!("Could not find path for {}", self.name);
+                Err(TrackError::NoTrackLocation)
+            }
+        }
     }
 
     async fn get_context_from_list(
         self_list: Vec<&Self>,
         index: usize,
         library: &Library,
-    ) -> Result<Option<QueueItem>> {
+    ) -> TrackResult<QueueItem> {
         let mut result = VecDeque::new();
         for track in self_list.into_iter() {
-            match track.get_context(library).await? {
-                Some(location) => result.push_back(location),
-                None => warn!("Could not find path for {}", track.name),
+            match track.get_context(library).await {
+                Ok(location) => result.push_back(location),
+                Err(err) => match err {
+                    // a track_location missing is not fatal
+                    TrackError::NoTrackLocation => (),
+                    TrackError::DbErr(db_err) => return Err(db_err.into()),
+                    // can't happen
+                    TrackError::WrongInput(err) => error!("WrongInput: {err}"),
+                },
             }
         }
         if index > result.len() {
             error!("index out of bounds while getting context");
-            return Err(DbErr::Custom("index out of bounds".into()));
+            return Err(TrackError::WrongInput("index out of bounds".into()));
         }
         result.rotate_left(index);
-        Ok(Some(QueueItem::PlayList(result, QueueOptions::default())))
+        Ok(QueueItem::PlayList(result, QueueOptions::default()))
     }
 }
 
@@ -63,19 +69,19 @@ macro_rules! standerd_list_context {
             self_list: Vec<&Self>,
             index: usize,
             library: &Library,
-        ) -> Result<Option<QueueItem>> {
+        ) -> TrackResult<QueueItem> {
             if let Some(item) = self_list.get(index) {
                 item.get_context(library).await
             } else {
                 error!("index out of bounds while getting context");
-                Err(DbErr::Custom("index out of bounds".into()))
+                Err(TrackError::WrongInput("index out of bounds".into()))
             }
         }
     };
 }
 
 impl GetContext for release::Model {
-    async fn get_context(&self, library: &Library) -> Result<Option<QueueItem>> {
+    async fn get_context(&self, library: &Library) -> TrackResult<QueueItem> {
         let tracks = self
             .find_related(track::Entity)
             .find_also_related(track_location::Entity)
@@ -88,13 +94,13 @@ impl GetContext for release::Model {
                 None => warn!("Could not find path for {}", track.name),
             }
         }
-        Ok(Some(QueueItem::Album(result, QueueOptions::default())))
+        Ok(QueueItem::Album(result, QueueOptions::default()))
     }
     standerd_list_context!();
 }
 
 impl GetContext for artist::Model {
-    async fn get_context(&self, library: &Library) -> Result<Option<QueueItem>> {
+    async fn get_context(&self, library: &Library) -> TrackResult<QueueItem> {
         let discography = self
             .find_related(release::Entity)
             .all(&library.database)
@@ -105,10 +111,25 @@ impl GetContext for artist::Model {
         }
         result
             .into_iter()
-            .collect::<Result<Option<VecDeque<_>>>>()
-            .map(|option| option.map(|vec| QueueItem::PlayList(vec, QueueOptions::default())))
+            .collect::<TrackResult<VecDeque<_>>>()
+            .map(|vec| QueueItem::PlayList(vec, QueueOptions::default()))
     }
     standerd_list_context!();
 }
 
-pub type Result<T, E = migration::DbErr> = std::result::Result<T, E>;
+pub type TrackResult<T, E = TrackError> = std::result::Result<T, E>;
+
+pub enum TrackError {
+    /// There was no track_location for this track in the database
+    NoTrackLocation,
+    /// The input was faulty
+    WrongInput(String),
+    /// The database gave an error
+    DbErr(migration::DbErr),
+}
+
+impl From<migration::DbErr> for TrackError {
+    fn from(value: migration::DbErr) -> Self {
+        Self::DbErr(value)
+    }
+}
